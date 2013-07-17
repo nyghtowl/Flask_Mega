4f49517306428c2c26ede7726239150f2f1cd0d2
nyghtowl <warrick.melanie@gmail.com>
2013-07-17 14:05:04 -0700

    Building out view with embedded html.

--- app/views.py
+++ app/views.py
@@ -3,5 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hellow World!"
-
+    user = { 'nickname': 'Miguel' } # fake user
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
