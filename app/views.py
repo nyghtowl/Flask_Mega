0dd251ccbe3dd4902b87103a28eaf983bce63831
nyghtowl <warrick.melanie@gmail.com>
2013-07-18 15:29:36 -0700

    Added user function with route for user page.

--- app/views.py
+++ app/views.py
@@ -33,7 +33,23 @@ def index():
         title = 'Home', 
         user = user,
         posts = posts)
-    
+  
+@app.route('/user/<nickname>')
+@login_required # Restricts page access without login
+    def user(nickname):
+        user = User.query.filter_by(nickname = nickname).first()
+        if user == None:
+            flash('User' + nickname + 'not found.')
+            return redirect(url_for('index'))
+        posts = [
+            { 'author': user, 'body': 'Test post #1' },
+            { 'author': user, 'body': 'Test post #1' }
+        ]
+        return render_template("user.html", 
+            user = user,
+            posts = posts)
+ 
+
 @app.route('/login', methods = ['GET', 'POST'])
 @oid.loginhandler
 def login():
