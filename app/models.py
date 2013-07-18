617f85db0a6a997b06c403e6cfe16f4247ad4e5d
nyghtowl <warrick.melanie@gmail.com>
2013-07-17 17:51:23 -0700

    Adding methods for User model based on Flask-Login extension requirements.

--- app/models.py
+++ app/models.py
@@ -11,6 +11,22 @@ class User(db.Model):
 	role = db.Column(db.SmallInteger, default = ROLE_USER)
 	posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 
+	# Return true unless user not allowed to authenticate
+	def is_authenticated(self):
+        return True
+
+    # Returns False for a banned user
+    def is_active(self):
+        return True
+
+    # Return true for fake users not supposed to login
+    def is_anonymous(self):
+        return False
+
+    # Generate unique id for user 
+    def get_id(self):
+        return unicode(self.id)
+
 # Used fro debugging and structures how to print objects fo this class
 def __repr__(self):
 	return '<User %r>' % (self.nickname)
