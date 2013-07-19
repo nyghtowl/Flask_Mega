a2c38089bd138437de24113583e2767766342112
nyghtowl <warrick.melanie@gmail.com>
2013-07-18 22:05:24 -0700

    Added test for follower db and methods.

--- test.py
+++ test.py
@@ -30,12 +30,48 @@ class TestCase(unittest.TestCase):
         db.session.commit()
         nickname = User.make_unique_nickname('john')
         assert nickname != 'john'
+
         u = User(nickname = nickname, email = 'susan@example.com')
         db.session.add(u)
         db.session.commit()
         nickname2 = User.make_unique_nickname('john')
         assert nickname2 != 'john'
         assert nickname2 != nickname
+
+    def test_follow(self):
+        u1 = User(nickname = 'john', email = 'john@example.com')
+        u2 = User(nickname = 'susan', email = 'susan@examplecom')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+
+        # Test that unfollow is none if they users haven't been linked 
+        assert u1.unfollow(u2) == None
+
+        u = u1.follow(u2)
+        db.session.add(u)
+        db.session.commit()
+
+        # Test followed and followers methods
+        assert u1.follow(u2) == None
+        assert u1.is_following(u2)
+        assert u1.followed.count() == 1
+        assert u1.followed.first().nickname == 'susan'
+        assert u2.followed.count() == 0
+        assert u2.followers.count() == 1
+        assert u2.followers.first().nickname = 'john'
+
+        u = u1.unfollow(u2)
+
+        # Confirm u has value
+        assert u != None
+        db.session.add(u)
+        db.session.commit()
+
+        # Test that unfollow method workds
+        assert u1.is_following(u2) == False
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
 
 if __name__ == '__main__':
     unittest.main()
