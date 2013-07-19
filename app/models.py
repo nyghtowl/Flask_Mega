cf7cc6fef8db3fdad98614c6e962a8670a811220
nyghtowl <warrick.melanie@gmail.com>
2013-07-18 18:01:13 -0700

    Added method to generate new nickname.

--- app/models.py
+++ app/models.py
@@ -14,6 +14,19 @@ class User(db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
 
+    @staticmethod
+    def make_unique_nickname(nickname):
+        if User.query.filter_by(nickname=nickname).first() == None:
+            return nickname
+        version = 2
+        while True:
+            new_nickname = nickname + str(version)
+            if User.query.filter_by(nickname = new.nickname).first() == None:
+                break
+            version += 1
+        return new_nickname
+
+
     # Return true unless user not allowed to authenticate
     def is_authenticated(self):
         return True
