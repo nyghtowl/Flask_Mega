70b85d6761b69dec6c241ba13d1601c2f3ae0ea5
nyghtowl <warrick.melanie@gmail.com>
2013-07-18 17:14:02 -0700

    Added exception handling with email to initalizer.

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
-from config import basedir
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 import os
 
 app = Flask(__name__)
@@ -16,4 +16,12 @@ oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
 
-
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
