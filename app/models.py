0251a9990d8e82fa16747f1322365e08b9f72daf
nyghtowl <warrick.melanie@gmail.com>
2013-07-18 21:37:35 -0700

    Fixed timestamp error.

--- app/models.py
+++ app/models.py
@@ -61,7 +61,7 @@ class User(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String(140))
-    timestamp = db. Column(db.DateTime)
+    timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
