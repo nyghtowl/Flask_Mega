038c04c6737c6f5a7b70fda595459b0810e6e131
nyghtowl <warrick.melanie@gmail.com>
2013-07-20 13:13:15 -0700

    Comment

--- app/email.py
+++ app/email.py
@@ -1,6 +1,7 @@
 from flask.ext.mail import Message
 from app import mail
 
+# Email support
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender = sender, recipients = recipients)
     msg.body = text_body
