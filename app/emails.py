bd888990d8a234e93b558bfe31f17b2153440bf2
nyghtowl <warrick.melanie@gmail.com>
2013-07-20 19:59:01 -0700

    Added threading to email.

--- app/emails.py
+++ app/emails.py
@@ -2,13 +2,18 @@ from flask import render_template
 from flask.ext.mail import Message
 from app import mail
 from config import ADMINS
+from threading import Thread
+
+def send_async_email(msg):
+    mail.send(msg)
 
 # Email support
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender = sender, recipients = recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    thr = Thread(target = send_async_email, args = [msg])
+    thr.start()
 
 # Emails followers
 def follower_notification(followed, follower):
