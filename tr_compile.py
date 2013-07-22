fe207b6215e3dc1f751d69c78c25301f280d7633
nyghtowl <warrick.melanie@gmail.com>
2013-07-21 22:39:08 -0700

    Short script to compile pybabel files.

---
+++ tr_compile.py
@@ -0,0 +1,9 @@
+#Pybabel script to compile and catalog
+#!flask/bin/python
+import os
+import sys
+if sys.platform == 'wn32':
+    pybabel = 'flask\\Scripts\\pybabel'
+else:
+    pybabel = 'flask/bin/pybabel'
+os.system(pybabel + ' compile -d app/translations')
